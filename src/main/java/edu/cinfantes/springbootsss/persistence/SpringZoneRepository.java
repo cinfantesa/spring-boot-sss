dd8272d6c1d96d3cc1903186bcc9897287ee0195
Cristobal Infantes <cinfantes@grupoasv.com>
2019-01-05 12:23:14 +0100

    security

--- src/main/java/edu/cinfantes/springbootsss/persistence/SpringZoneRepository.java
+++ src/main/java/edu/cinfantes/springbootsss/persistence/SpringZoneRepository.java
@@ -4,25 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
-@RepositoryRestResource(path = "zonas", excerptProjection = ZoneProjection.class)
 public interface SpringZoneRepository extends JpaRepository<Zone, UUID>, JpaSpecificationExecutor<Zone> {
-  @Override
-  <S extends Zone> List<S> saveAll(Iterable<S> iterable);
-
-  @RestResource(path = "prueba", rel = "prueba")
   List<ZoneProjection> findByNameContaining(String name);
   List<ZoneProjection> findAllByOrderByNameDesc();
   List<ZoneProjection> findAllByPriority(int priority);
   List<ZoneProjection> findAllByCreatedBetween(Date from, Date to);
 
-  @RestResource(exported = false)
   @Query("select z from Zone z order by z.polygons.size desc")
   List<ZoneProjection> findAllByPolygonsSizeDesc();
 
--- src/main/java/edu/cinfantes/springbootsss/persistence/Zone.java
+++ src/main/java/edu/cinfantes/springbootsss/persistence/Zone.java
@@ -9,7 +9,6 @@ import org.hibernate.annotations.Type;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
@@ -38,7 +37,6 @@ public class Zone {
   )
   private UUID id;
 
-  @RestResource(exported = false)
   @CreatedDate
   private Date created;
 
